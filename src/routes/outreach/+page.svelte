61356bc06de3302e0160eb479728adbb5d7bbbe8
Kireeti Kompella <127916417+brachiotusk@users.noreply.github.com>
2024-10-29 19:29:08 -0700

    Create +page.svelte
    added first outreach thing, will add imgs from google drive soon

---
+++ src/routes/outreach/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { base } from "$app/paths";
+    import ExpandingHr from "../../lib/common/ExpandingHR.svelte";
+    import PageWrapper from "../../lib/common/PageWrapper.svelte";
+    import Header from "../../lib/layout/Header.svelte";
+</script>
+<Header/>
+<PageWrapper>
+    <h1 class="text-3xl font-semibold">Outreach</h1>
+    <ExpandingHr --width=10em/>
+    Over the past 5 years of Escape Velocity, we have built a large scale operation working to both bring FIRST and STEM to local communities but also across the globe.
+      Our longest partnership has been with the nonprofit Joy of Reading. Joy of Reading is a 501c(3) nonprofit that works to donate used books and set up libraries across
+      the globe to support disadvantaged kids. They have started 165+ Libraries across India, Nepal, Costa Rica and soon parts of Africa, along with this they have donated upwards of 45,000 books. We worked
+      with Joy of Reading to start a library in Haryana, 2 years ago and have worked at multiple book sorting and collecting events across King County. This year, we are starting a library in Hyderabad, India,
+      and helping to support the newly started computer lab at a school in Vijaywada, India. We will work with them to begin FIRST in the coming years. 
+</PageWrapper>
